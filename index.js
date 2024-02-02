9f3be40a8e0cb5211efd25cccfe8646e061beb95
yavrum13 <yuval.avrum@gmail.com>
2024-02-02 00:43:56 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // code your solution here
+function superbowlWin(record) {
+    const winningGame = record.find(game => game.result === 'W');
+    return winningGame ? winningGame.year : undefined;
+  }
